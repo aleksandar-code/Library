0e85b5d9e7c2661d74cadb05a6bbba0c46ee0227
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-03-04 14:16:39 +0100

    Can create show form when new book button is clicked

---
+++ app.js
@@ -0,0 +1,37 @@
+let myLibrary = [];
+
+class Book {
+    constructor(author, title, pages, read) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+function addBookToLibrary() {
+    
+}
+
+function showForm() {
+    form.style.display = "block";
+    newBook.style.display = "none";
+}
+
+let newBook = document.getElementById("new-book");
+
+let form = document.getElementsByTagName("form")[0];
+
+let submitButton = document.querySelector('button[type=submit]');
+
+
+form.style.display = "none";
+
+submitButton.onclick = () => addBookToLibrary();
+
+
+newBook.onclick = () => showForm();
+
+
+
+
